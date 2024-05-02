6217182aa6960a6ca07ca05981a25eadcf26aeee
crisacost <acostaacristina@gmail.com>
2024-05-02 10:06:54 -0300

    segundapreentregaA

--- main.js
+++ main.js
@@ -8,9 +8,56 @@ while( usuario == " " ){                               /* ver de poner tambien u
 /* ver si cambiar el script de lugar abajo de todo, en el body para que aparezca 
 el html antes de terminar toda la ejecucion de js */
 
+
+/*objetos contructores/ funciones contructoras*/
+
+const lugaresDisponibles = {
+    auto: 100,
+    moto: 100,
+    chata: 100,
+};
+
+class Vehiculo {                      /*no era mi intencion poner una clase pero pide esa correcion (no se porque)*/
+    constructor(marca, patente, tipo) {
+        this.marca = marca;
+        this.patente = patente;
+        this.tipo = tipo;
+    }
+}
+
+function filtrarPorTipo(vehiculos, tipo) {
+    return vehiculos.filter(vehiculo => vehiculo.tipo === tipo);
+}
+
+function contarVehiculos() {
+    for (const vehiculo of vehiculosEstacionados) {
+        lugaresDisponibles [vehiculo.tipo] --;
+    }
+}
+
+/*Arrays + funciones y metodos*/
+const vehiculosEstacionados = [];
+
+function agregarvehiculo (vehiculo) {
+    vehiculosEstacionados.push(vehiculo);
+}
+
+function removerVehiculo (patente) {
+    const indice = vehiculosEstacionados.findIndex(vehiculo => vehiculo.patente === patente);
+    if (indice !== -1) {
+        const vehiculoRemovido = vehiculosEstacionados.splice(indice, 1)[0];
+        lugaresDisponibles[vehiculoRemovido.tipo]++;
+        return vehiculoRemovido;
+    } else {
+        return null;
+    }
+
+}
+
+
 let techo = prompt("Bienvenido " + usuario + " elija una opcion: bajotecho - sintecho ")
 
-if(techo == "bajotecho"){
+if(techo === "bajotecho"){
     alert (" Elejiste bajotecho");
     bajotecho();
 
@@ -21,19 +68,37 @@ if(techo == "bajotecho"){
 /* Funciones*/
 
 function bajotecho() {
-    let vehiculo = prompt(" Dinos tu vehiculo: Auto-Motocicleta-Chata")
+    let vehiculo = prompt(" Dinos tu vehiculo: auto-moto-chata")
 
     switch (vehiculo) {
         case "auto":
-            alert("Elejiste auto, tenes lugar en el primer piso. Gracias");
+            if (lugaresDisponibles.auto > 0) {
+                agregarvehiculo(new Vehiculo("Desc", "Desc", "auto"));
+                lugaresDisponibles.auto--;
+                alert("Elejiste auto, tenes lugar en el primer piso. Gracias");
+            } else {
+                alert("Lo sentimos, no hay lugares disponibles");
+            }
             break;
 
-        case "motocicleta":
-            alert("Elejiste motocicleta, tenes lugar en planta baja B. Gracias");
+        case "moto":
+            if (lugaresDisponibles.moto > 0) {
+                agregarvehiculo(new Vehiculo("Desc", "Desc", "moto"));
+                lugaresDisponibles.moto--;
+                alert("Elejiste moto, tenes lugar en planta baja B. Gracias");
+            } else {
+                alert("Lo sentimos, no hay lugares disponibles");
+            }
             break;
 
         case "chata":
-            alert("Elejiste Chata, tenes lugar en el segundo piso. Gracias");
+            if (lugaresDisponibles.chata > 0) {
+                agregarvehiculo(new Vehiculo("Desc", "Desc", "chata"));
+                lugaresDisponibles.chata--;
+                alert("Elejiste Chata, tenes lugar en el segundo piso. Gracias");
+            } else {
+                alert("Lo sentimos, no hay lugares disponibles");
+            }
             break;
         default:
             alert("No es una opcion valida");
@@ -42,7 +107,16 @@ function bajotecho() {
     }
 
 }
- 
+
+contarVehiculos();
+
+console.log("Lugares disponible para autos:", lugaresDisponibles.auto);
+console.log("Lugares disponible para moto:", lugaresDisponibles.moto);
+console.log("Lugares disponible para chata:", lugaresDisponibles.chata);
+
+/*...........................................................................................................*/
+
+
 
 
 
